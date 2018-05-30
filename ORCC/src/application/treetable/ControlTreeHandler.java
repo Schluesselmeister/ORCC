3ffadc3efde63f7885778995fe1ccb1b674a66c1
Schluesselmeister <development@rehfuss.org>
2018-05-30 16:11:07 +0200

    Update

---
+++ ORCC/src/application/treetable/ControlTreeHandler.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package application.treetable;
+
+import javafx.collections.ObservableList;
+import javafx.scene.control.TreeView;
+import openhab.OpenhabItem;
+
+/**
+ * @author Sven Rehfuﬂ
+ *
+ */
+public class ControlTreeHandler {
+
+	
+	
+	public ControlTreeHandler(TreeView localControllTree, TreeTableHandler treeTableHandler) {
+		
+	}
+	
+}
